4f022f837aa312802985dc7d1b5c97b9e2ed7473
agalisteojdam1 <agalisteojdam1@gmail.com>
2022-01-21 16:27:11 +0100

    Realizado método tieneLetrasRepetidas(String cadena) de la clase utilidades)

--- Utilidades.java
+++ Utilidades.java
@@ -4,7 +4,7 @@
  *
  */
 public class Utilidades {
-    
+
     /**
      * Dada una cadena devuelve una nueva capitalizando los caracteres de
      * tres en tres de forma
@@ -14,14 +14,13 @@ public class Utilidades {
      * si la cadena recibida es "zapato" devuelve "ZAPato"
      * si la cadena recibida es "de" devuelve "DE"
      */
-    
+
     public static String capitalizarAlterna(String cadena) {
         //TODO
-        
+
         return null;
     }
-    
-    
+
     /**
      * Dada una cadena devuelve true si hay letras repetidas, false en otro caso
      * Es indiferente mayúsculas o minúsculas
@@ -30,49 +29,65 @@ public class Utilidades {
      * indexOf y substring
      *
      */
-    public static boolean tieneLetrasRepetidas(String cadena) {
-        //TODO
-        
+    public static  boolean tieneLetrasRepetidas(String cadena) {
+        cadena = cadena.toLowerCase();
+        while(cadena.length() > 0){
+            String ultimaLetra = cadena.substring(cadena.length() - 1);
+            cadena = cadena.substring(0,cadena.length() - 1);
+
+            if(estaLetra(ultimaLetra,cadena)){
+                return true;
+            }
+        }
+
         return false;
     }
-    
-    
-    
-    
+
     /**
-     *
+     * Especifica si una letra esta en una frase o no.
      */
-    public static void main(String[] args) {
-        String cadena = "zaPaTo";
-        System.out.println(cadena + "\tCapitalizada alterna: " +
-                Utilidades.capitalizarAlterna(cadena));
-        cadena = "pez";
-        System.out.println(cadena + "\tCapitalizada alterna: " +
-                Utilidades.capitalizarAlterna(cadena));
-        
-        cadena = "vaso";
-        System.out.println(cadena + "\tCapitalizada alterna: " +
-                Utilidades.capitalizarAlterna(cadena));
-        
-        
-        cadena = "semana";
-        System.out.println(cadena + "\tTiene letras repetidas?: " +
-                Utilidades.tieneLetrasRepetidas(cadena));
-        
-        cadena = "quebrantos";
-        System.out.println(cadena + "\tTiene letras repetidas?: " +
-                Utilidades.tieneLetrasRepetidas(cadena));
-        
-        cadena = "y";
-        System.out.println(cadena + "\tTiene letras repetidas?: " +
-                Utilidades.tieneLetrasRepetidas(cadena));
-        
-        cadena = "de";
-        System.out.println(cadena + "\tTiene letras repetidas?: " +
-                Utilidades.tieneLetrasRepetidas(cadena));
-        
-       
-        
+    private static boolean estaLetra(String letra, String cadena)
+    {
+        while(cadena.length() > 0){
+            String ultimaLetra = cadena.substring(cadena.length() - 1);
+            cadena = cadena.substring(0,cadena.length() - 1);
+            if(ultimaLetra.compareTo(letra) == 0){
+                return true;
+            }
+        }
+        return false;
     }
-    
+
+    /**
+    *
+    */
+    public static void main(String[] args) {
+    String cadena = "zaPaTo";
+    System.out.println(cadena + "\tCapitalizada alterna: " +
+    Utilidades.capitalizarAlterna(cadena));
+    cadena = "pez";
+    System.out.println(cadena + "\tCapitalizada alterna: " +
+    Utilidades.capitalizarAlterna(cadena));
+
+    cadena = "vaso";
+    System.out.println(cadena + "\tCapitalizada alterna: " +
+    Utilidades.capitalizarAlterna(cadena));
+
+    cadena = "semana";
+    System.out.println(cadena + "\tTiene letras repetidas?: " +
+    Utilidades.tieneLetrasRepetidas(cadena));
+
+    cadena = "quebrantos";
+    System.out.println(cadena + "\tTiene letras repetidas?: " +
+    Utilidades.tieneLetrasRepetidas(cadena));
+
+    cadena = "y";
+    System.out.println(cadena + "\tTiene letras repetidas?: " +
+    Utilidades.tieneLetrasRepetidas(cadena));
+
+    cadena = "de";
+    System.out.println(cadena + "\tTiene letras repetidas?: " +
+    Utilidades.tieneLetrasRepetidas(cadena));
+
+    }
 }
