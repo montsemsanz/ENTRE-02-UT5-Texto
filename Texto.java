9af595591017066ecae3bddc774034361a9abf97
jlincdam1 <jlincdam1@gmail.com>
2022-01-24 20:24:13 +0100

    Terminado el método calcularFrecuenciaLongitud()

--- Texto.java
+++ Texto.java
@@ -201,7 +201,21 @@ public class Texto {
      *
      */
     public int[] calcularFrecuenciaLongitud() {
-        
+        int[] frecPalabras = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int[] longitudes = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        int j = 0;
+        int i = 0;
+        while(j < 15){
+            while(i < total){
+                if(palabras[i].getPalabra().length() == longitudes[j]){
+                    frecPalabras[j]++;
+                }
+                i++;
+            }
+            i = 0;
+            j++;
+        }
+        return frecPalabras;
     }
     
     /**
